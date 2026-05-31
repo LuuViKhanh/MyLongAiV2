dc278ccdc763de967b140316b5ec959213df1f35
LuuViKhanh <karlpro812005@gmail.com>
2026-05-31 12:45:18 +0700

    Update project structure

--- app/core/config.py
+++ app/core/config.py
@@ -3,8 +3,6 @@ from pydantic_settings import BaseSettings
 class Settings(BaseSettings):
     APP_NAME: str = "MyLongAI"
     DEBUG: bool = True
-    MODEL_PATH: str = "app/models/my_model.pt"
-    MAX_IMAGE_SIZE: int = 2 * 1024 * 1024  # 2MB
     MYSQL_URL: str = "mysql+pymysql://root:password@localhost:3306/weather_db"
 
     class Config:
--- app/main.py
+++ app/main.py
@@ -1,6 +1,6 @@
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
-from app.api.routes import ai_detect, ai_realtime, health, weather
+from app.api.routes import health, weather
 from app.models.weather_record import create_tables
 
 app = FastAPI(
@@ -23,8 +23,6 @@ app.add_middleware(
     allow_headers=["*"],
 )
 
-app.include_router(ai_detect.router, prefix="/ai", tags=["AI"])
-app.include_router(ai_realtime.router, prefix="/ai", tags=["AI Realtime"])
 app.include_router(health.router, prefix="/health", tags=["Health"])
 app.include_router(weather.router, prefix="/weather", tags=["Weather AI"])
 @app.get("/")
